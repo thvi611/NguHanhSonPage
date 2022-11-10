be652cc35e31cc4a26f38c1b19d93d7a6b7b0ce2
thvi611 <lovivivi611@gmail.com>
2022-11-10 10:05:39 +0700

    Xem chi tiet va tao bai viet

--- it-page/src/App.js
+++ it-page/src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import PostDetail from './Page/PostDetail/PostDetail';
+import CreatePost from './Page/CreatePost/CreatePost';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PostDetail></PostDetail>
+      {/* <CreatePost></CreatePost> */}
     </div>
   );
 }
