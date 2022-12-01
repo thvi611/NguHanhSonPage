39374bd6042503b12c883c72fc6e40b249043053
thvi611 <lovivivi611@gmail.com>
2022-12-01 13:57:42 +0700

    add image of post

--- backend-app/app/Http/Controllers/PostController.php
+++ backend-app/app/Http/Controllers/PostController.php
@@ -85,7 +85,7 @@ class PostController extends Controller
     public function show(Post $post)
     {
         //
-        return $post->load('categories','comments');
+        return $post->load('categories','comments','images');
     }
 
     /**
